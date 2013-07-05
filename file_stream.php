105b87c3a38d0fab7ec160b591bee4c9df733409
tanakayuki <a0673040@gmail.com>
2013-07-05 15:21:43 +0900

    update file_stream.php

--- file_stream.php
+++ file_stream.php
@@ -9,43 +9,43 @@ require './vendor/autoload.php';
 $client = new Elastica\Client(array(
 	'url' => 'http://localhost:9200/',
 ));
-$client->setConfig(array('log' => true));
 $client->setConfig(array('timeout' => 10));
 $client->setConfig(array('connections' => array(
     'timeout' => 10
 )));
 $chat_type = $client->getIndex('cw')->getType('chat');
 
-$fp = fopen($testdata_file, 'r');
-if ($fp) {
-    $documents = array();
-    while (!feof($fp)) {
-        $text = fgets($fp);
-        if (!isset($text)) {
-            continue;
-        }
-        $data = array(
-            'message' => $text,
-            'rid' => rand(1, 30),
-            'aid' => rand(1, 50),
-            'create_date' => date('Y-m-d H:i:s')
-        );
-        $documents[] = new Elastica\Document('', $data);
-        echo print_r($data, true);
-        if (count($documents) >= 20) {
-            echo "Trying to put documents.....\n";
-            try {
-                $start_time = microtime(true);
-                $chat_type->addDocuments($documents);
-                $end_time = microtime(true);
-
-                echo "###### Time : " . ($end_time - $start_time) . "\n";
-                echo "###### (insert " . count($documents) . " data) \n";
-            } catch (\Exception $e) {
-                echo $e . "\n";
+while (1) {
+    $fp = fopen($testdata_file, 'r');
+    $insert_cnt  = isset($argv[2]) ? $argv[2] : 100;
+    if ($fp) {
+        $documents = array();
+        while (!feof($fp)) {
+            $text = fgets($fp);
+            if (!isset($text)) {
+                continue;
             }
-            $documents = array();
+            $data = array(
+                'message' => $text,
+                'rid' => rand(1, 30),
+                'aid' => rand(1, 50),
+                'create_date' => date('Y-m-d H:i:s')
+            );
+            $documents[] = new Elastica\Document('', $data);
+            if (count($documents) >= $insert_cnt) {
+                try {
+                    $start_time = microtime(true);
+                    $chat_type->addDocuments($documents);
+                    $end_time = microtime(true);
+    
+                    echo "###### Time : " . ($end_time - $start_time) . "\n";
+                    echo "###### (insert " . count($documents) . " data) \n";
+                } catch (\Exception $e) {
+                    echo $e . "\n";
+                }
+                $documents = array();
+            }
         }
+        fclose($fp);
     }
-    fclose($fp);
 }
